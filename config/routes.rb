4592a512dc4fff5449b617a7a06a7cad8bdc99fd
kmohanraj <mohanksv.89@gmail.com>
2018-02-23 11:16:26 +0530

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
 
   root "homes#index"
   
-  devise_for :users
-  # devise_for :users, controllers: {sessions: 'users/sessions'}
+  devise_for :users, controllers: {sessions: 'users/sessions'}
 
 
   resources :homes
